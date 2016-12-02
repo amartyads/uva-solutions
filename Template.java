3a989e3dcb29153aa98c07816fd2e734357f3571
Amartya Das Sharma <amartyads@gmail.com>
2016-12-02 15:04:54 +0400

    Java template added

---
+++ Template.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+public class Template
+{
+    public static void main(String[] args)throws IOException
+    {
+        //System.setIn(new FileInputStream(new File("1in.txt")));
+        //System.setOut(new PrintStream(new File("1out.txt")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
